8353c81bca3d50ab3555afaf295a0e21c24b1fd8
KingTutankhamun <65089978+KingTutankhamun@users.noreply.github.com>
2022-01-15 21:40:22 -0800

    Add files via upload
    Added bottle height and bottle area calibration functions

--- HydroHomie.ino
+++ HydroHomie.ino
@@ -10,28 +10,24 @@ void setup() {
 
 const int MEASURE_TIMES = 30;
 const int SOUND_SPEED = 34300; //cm/second
-const int bottle_height = 20;
-const int bottle_area = 35;
+const int CALIBRATION_VOLUME = 100; //volume, in mL, that is used for calibration
+int bottle_height = 20;
+int bottle_area = 35;
+
 void loop() {
-  int water_volume = measureWater(bottle_height, bottle_area);
-  Serial.println("Water Volume: " + String(water_volume));
+  /*int water_volume = measureWater();
+  Serial.println("Water Volume: " + String(water_volume));*/
+
+  calibrateHeight();
+  Serial.println(String(bottle_height));
   delay(1000);
 }
 
 
 //takes in bottle parameters, returns amount of water in bottle (in mL)
 //returns -1 if the cap has been removed
-int measureWater(int bottle_height, int bottle_area) {
-  int sum = 0;
-  for (int i=0;i<MEASURE_TIMES; i++) {
-    digitalWrite(trigPin, LOW);
-    delayMicroseconds(2);
-    digitalWrite(trigPin, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(trigPin, LOW);
-    sum += pulseIn(echoPin, HIGH);
-  }
-  int average_time = sum / MEASURE_TIMES;
+int measureWater() {
+  int average_time = measureTime();
 
   //water_height in cm
   int water_height = bottle_height - SOUND_SPEED * average_time / 2000000;
@@ -41,4 +37,37 @@ int measureWater(int bottle_height, int bottle_area) {
   
   int volume = bottle_area * water_height;
   return volume;
+}
+
+//uses ultrasonic sensor, returns average value over MEASURE_TIMES trials
+int measureTime() {
+  int sum = 0;
+  for (int i=0;i<MEASURE_TIMES; i++) {
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+    sum += pulseIn(echoPin, HIGH);
+  }
+  return sum / MEASURE_TIMES;
+}
+
+//sets the bottle height, used when the bottle is empty
+void calibrateHeight() {
+  int average_time = measureTime();
+  bottle_height = SOUND_SPEED * average_time / 2000000;
+  
+  return;
+}
+
+//sets the bottle area, used when the bottle has CALIBRATION_VOLUME water inside
+void calibrateArea() {
+  int average_time = measureTime();
+  int water_height = bottle_height - SOUND_SPEED * average_time / 2000000;
+  
+  //CALIBRATION_VOLUME (in mL) / height (in cm) = area (in cm^2)
+  bottle_area = CALIBRATION_VOLUME / water_height;
+  
+  return;
 }
